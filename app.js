e7934048a5914f4ec5365447a1580e9f189fa704
attackpiggy <s.yusuf91@hotmail.com>
2019-04-10 12:32:13 -0400

    adding the js

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',()=>
+{
+    let long;
+    let lat;
+
+    if (navigator.geolocation){
+
+    }else{
+        h1.textContent="hey this is not working, reload the page and allow the popup to so that it can find the location properly"
+    }
+
+});
