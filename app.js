11298f144e4a23b7a95e47c11e8829b4be9a0ee3
attackpiggy <s.yusuf91@hotmail.com>
2019-04-12 00:06:07 -0400

    small changes

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ window.addEventListener('load',()=>
     let long;
     let lat;
 
-    if (navigator.geolocation){
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(position=>{
+            long=position.coords.longitude
+            lat=postion.coords.latitude;
+        })
 
     }else{
         h1.textContent="hey this is not working, reload the page and allow the popup to so that it can find the location properly"
