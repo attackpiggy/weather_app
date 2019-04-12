ba4d30e2695a9b96cd263469a29777bad6481184
attackpiggy <s.yusuf91@hotmail.com>
2019-04-12 00:09:52 -0400

    small changes

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ window.addEventListener('load',()=>
     if (navigator.geolocation)
     {
         navigator.geolocation.getCurrentPosition(position=>{
-            long=position.coords.longitude
+            long=position.coords.longitude;
             lat=postion.coords.latitude;
-        })
+            
+            const api=`https://api.darksky.net/forecast/c23b7e07bc7a9bbe0f187be1473a5105/37.8267,-122.4233`
+        });
 
     }else{
         h1.textContent="hey this is not working, reload the page and allow the popup to so that it can find the location properly"
