e2d6599a57135efc72d5aa990ace4028fece094a
attackpiggy <s.yusuf91@hotmail.com>
2019-04-12 00:29:55 -0400

    minor changes to check whether json data is coming or not

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ window.addEventListener('load',()=>
 {
     let long;
     let lat;
+    let temperatureDescription=document.querySelector('.temperature-description');
+    let temperatureDegree=document.querySelector('.temperature-description');
 
     if (navigator.geolocation)
     {
@@ -10,13 +12,15 @@ window.addEventListener('load',()=>
             lat=postion.coords.latitude;
             
             const proxy='https://cors-anywhere.heroku.com/'
-            const api=`https://api.darksky.net/forecast/c23b7e07bc7a9bbe0f187be1473a5105/${lat},${long}`;
+            const api=`${proxy}https://api.darksky.net/forecast/c23b7e07bc7a9bbe0f187be1473a5105/${lat},${long}`;
             fetch(api)
             .then(response=>{
                 return response.json();
             })
             .then(data=>{
                 console.log(data);
+                const{temperature,summary}=data.currently;
+
             });
         // else{
         //     h1.textContent="hey this is not working, reload the page and allow the popup to so that it can find the location properly"
