69854fb8fda828641005b0e525131674ac0d880d
attackpiggy <s.yusuf91@hotmail.com>
2019-04-12 00:20:48 -0400

    making api calls

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ window.addEventListener('load',()=>
             long=position.coords.longitude;
             lat=postion.coords.latitude;
             
+            const proxy='https://cors-anywhere.heroku.com/'
             const api=`https://api.darksky.net/forecast/c23b7e07bc7a9bbe0f187be1473a5105/${lat},${long}`;
+            fetch(api)
+            .then(response=>{
+                return response.json();
+            })
+            .then(data=>{
+                console.log(data);
+            });
+        // else{
+        //     h1.textContent="hey this is not working, reload the page and allow the popup to so that it can find the location properly"
+        
         });
-
-    }else{
-        h1.textContent="hey this is not working, reload the page and allow the popup to so that it can find the location properly"
     }
 
 });
